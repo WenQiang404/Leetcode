0dc63813b934ecc67636225396e44ccd0f50c570
WenQiang404 <wenqiang9946@163.com>
2023-03-25 18:58:16 +0800

    更新第26题

--- Golang/Array/26-删除数组中的重复项.go
+++ Golang/Array/26-删除数组中的重复项.go
@@ -1,4 +1,5 @@
-/*给你一个 升序排列 的数组 nums ，请你原地删除重复出现的元素使每个元素只出现一次 ，
+/*
+给你一个 升序排列 的数组 nums ，请你原地删除重复出现的元素使每个元素只出现一次 ，
 返回删除后数组的新长度.元素的相对顺序应该保持一致 。
 由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
 将最终结果插入 nums 的前 k 个位置后返回 k 。
@@ -6,7 +7,6 @@
 
 删除前	[1,2,3,3,4,5,5,6]
 删除后	[1,2,3,4,5,6]
-
 */
 package main
 
@@ -25,7 +25,7 @@ func removeDuplicates(nums []int) (int, []int) {
 		}
 	}
 	length := len(numMap)
-	var arraySort   []int
+	var arraySort []int
 	for _, value := range numMap {
 		arraySort = append(arraySort, value)
 	}
